07bf9104d1ec14d40989b91185e43f202abcf664
Lewis Jones <81636428+LewisJones2021@users.noreply.github.com>
2022-11-19 13:34:28 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+/** @format */
+
+(function () {
+ let screen = document.querySelector('.screen');
+ let buttons = document.querySelectorAll('.btn');
+ let clear = document.querySelector('.btn-clear');
+ let equal = document.querySelector('.btn-equal');
+
+ //check to see what value was pressed and display//
+ buttons.forEach(function (button) {
+  button.addEventListener('click', function (e) {
+   let value = e.target.dataset.num;
+   screen.value += value;
+  });
+ });
+
+ //equal button//
+ equal.addEventListener('click', function (e) {
+  if (screen.value === '') {
+   screen.value = 'Please enter';
+  } else {
+   let answer = eval(screen.value);
+   screen.value = answer;
+  }
+ });
+
+ //clear button//
+ clear.addEventListener('click', function (e) {
+  screen.value = '';
+ });
+})();
